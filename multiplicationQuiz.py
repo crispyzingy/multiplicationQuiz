dd5d8ce6abc7f533217f40ca4ba901fcb80bafed
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-09 23:46:43 +0530

    v1.0

--- multiplicationQuiz.py
+++ multiplicationQuiz.py
@@ -1,10 +1,44 @@
 #! python3
 # multiplicationQuiz - A number of multiplication questions are asked and user has to give the answers.
+import pyinputplus as pyip
+import random, time
 
-# TODO: No. of questions
+# No. of questions
+numberOfQuestions = 10
+correctAnswers = 0
 
-# TODO: Pick two random numbers
+for questionNumber in range(numberOfQuestions):
+    # Pick two random numbers:
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
 
-# TODO: Calculation
+    prompt = "#%s: %s x %s = " % (questionNumber + 1, num1, num2)
 
-# TODO: Scoreboard
+    # Validation
+    try:
+        # Right answers are handled by allowRegexes.
+        # Wrong answers are handled by blockRegexes, with a custom message.
+
+        pyip.inputStr(
+            prompt,
+            allowRegexes=["^%s$" % (num1 * num2)],
+            blockRegexes=[(".*", "Incorrect!")],
+            timeout=8,
+            limit=3,
+        )
+
+    except pyip.TimeoutException:
+        print("Out of time!")
+    except pyip.RetryLimitException:
+        print("Out of tries!")
+
+    else:
+        # This block runs if no exceptions were raised in the try block.
+        print("Correct!")
+        correctAnswers += 1
+
+    # Scoreboard
+    time.sleep(1)  # Brief pause to let user see the result
+
+print("Score: %s/ %s" % (correctAnswers, numberOfQuestions))
+

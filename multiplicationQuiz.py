0beb97ed7d6e80b90d828e170ff4c6bca88e445e
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-09 23:06:38 +0530

    initial commit

---
+++ multiplicationQuiz.py
@@ -0,0 +1,10 @@
+#! python3
+# multiplicationQuiz - A number of multiplication questions are asked and user has to give the answers.
+
+# TODO: No. of questions
+
+# TODO: Pick two random numbers
+
+# TODO: Calculation
+
+# TODO: Scoreboard
